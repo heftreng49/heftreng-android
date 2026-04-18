2bd274f5137fddf9705aae165fbf572015ede9e4
heftreng49 <siirgibi49@gmail.com>
2026-04-18 15:05:07 +0300

    heftreng-v2 tam proje

--- app/src/main/java/com/heftreng/app/model/HeftUser.java
+++
@@ -1,6 +0,0 @@
-package com.heftreng.app.model;
-public class HeftUser {
-    public String uid, displayName, username, bio, photoUrl;
-    public int postCount, followerCount, followingCount;
-    public HeftUser() {}
-}
