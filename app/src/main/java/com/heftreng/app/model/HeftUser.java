e3d9630b0e0d91faa52caa6db7a54d9753218c97
heftreng49 <siirgibi49@gmail.com>
2026-04-18 13:35:47 +0300

    Model sınıfları eklendi: Message, FeedPost, HeftUser

---
+++ app/src/main/java/com/heftreng/app/model/HeftUser.java
@@ -0,0 +1,6 @@
+package com.heftreng.app.model;
+public class HeftUser {
+    public String uid, displayName, username, bio, photoUrl;
+    public int postCount, followerCount, followingCount;
+    public HeftUser() {}
+}
