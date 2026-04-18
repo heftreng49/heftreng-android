3ebec8ab4f413406c0ed01054849bbcdc7206fc1
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2026-04-18 19:13:28 +0300

    Update FeedPost.java

--- app/src/main/java/com/heftreng/app/model/FeedPost.java
+++ app/src/main/java/com/heftreng/app/model/FeedPost.java
@@ -1,10 +1,20 @@
 package com.heftreng.app.model;
+
 import com.google.firebase.Timestamp;
 import java.util.List;
+
 public class FeedPost {
-    public String id,authorId,authorName,authorPhoto,content,imageUrl,type;
-    public int likeCount,commentCount;
+    public String id;
+    public String uid;
+    public String name;
+    public String photoURL;
+    public String text;
+    public String imgUrl;
+    public String ytVid;
+    public int likes;
+    public int saves;
+    public int cmtCount;
+    public Timestamp ts;
     public List<String> likedBy;
-    public Timestamp createdAt;
-    public FeedPost(){}
+    public FeedPost() {}
 }
