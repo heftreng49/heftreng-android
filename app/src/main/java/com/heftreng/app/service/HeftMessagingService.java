45a7c06d01153100461ccdf63f3988fb50d6e31d
heftreng49 <siirgibi49@gmail.com>
2026-04-19 09:30:32 +0300

    heftreng-v2-fixed kaynak kodu güncellendi

--- app/src/main/java/com/heftreng/app/service/HeftMessagingService.java
+++
@@ -1,7 +0,0 @@
-package com.heftreng.app.service;
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-public class HeftMessagingService extends FirebaseMessagingService {
-    @Override public void onMessageReceived(RemoteMessage r){}
-    @Override public void onNewToken(String t){}
-}
