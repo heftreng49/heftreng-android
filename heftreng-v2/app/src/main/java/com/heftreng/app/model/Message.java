1cf0c966c21506ed478e6e63658afa4d50b8c439
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2026-04-19 19:38:55 +0300

    Delete heftreng-v2 directory

--- heftreng-v2/app/src/main/java/com/heftreng/app/model/Message.java
+++
@@ -1,7 +0,0 @@
-package com.heftreng.app.model;
-public class Message {
-    public long id;
-    public String convId,fromUid,toUid,text,imageUrl,ts,replyToId,replyToText,replyToName;
-    public boolean isDeleted;
-    public boolean isMine(String myUid){return myUid!=null&&myUid.equals(fromUid);}
-}
