ca427a1461026609ed200522f4271dcb269525e8
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2026-04-29 12:48:10 +0300

    Delete app/src/main/java/com/chat directory

--- app/src/main/java/com/chat/Message.kt
+++
@@ -1,8 +0,0 @@
-package com.chat
-
-data class Message(
-    val senderId: String = "",
-    val text: String = "",
-    val timestamp: Long = System.currentTimeMillis(),
-    val seen: Boolean = false
-)
