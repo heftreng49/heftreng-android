79dee724fbec949a7502dd3652b8d5b90e7be36b
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2026-05-12 21:22:50 +0300

    Increment versionCode from 1 to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.heftreng.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
     }
 
