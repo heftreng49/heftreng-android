d7c0e3d34fe87a242c72456b1f64464901745e49
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2026-05-14 00:16:14 +0300

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.heftreng.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
     }
 
     signingConfigs {
