e3bfd51792b281a50e6b63a29230f219889f3395
heftreng49 <65556332+heftreng49@users.noreply.github.com>
2026-06-19 23:38:03 +0300

    Delete heftreng-android-main directory

--- heftreng-android-main/app/src/main/java/com/heftreng/app/ui/screens/serials/SerialsScreen.kt
+++
@@ -1,5 +0,0 @@
-package com.heftreng.app.ui.screens.serials
-
-// Bu dosya artık kullanılmıyor.
-// Tüm ekranlar com.heftreng.app.ui.screens.books.BookScreens içinde birleştirildi.
-// NavHost yönlendirmeleri güncellendi.
