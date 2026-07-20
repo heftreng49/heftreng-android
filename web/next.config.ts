356a6c410cb2c9c7431537cac43926950aa43f65
heftreng49 <siirgibi49@gmail.com>
2026-07-20 09:16:26 +0300

    fix: output export kaldirildi

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: {
-    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
