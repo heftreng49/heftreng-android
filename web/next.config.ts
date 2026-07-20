a29adb9fe53b52c7ff42536afd8ec48ea8b58a2d
heftreng49 <siirgibi49@gmail.com>
2026-07-20 09:12:09 +0300

    fix: next output export eklendi

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "firebasestorage.googleapis.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
