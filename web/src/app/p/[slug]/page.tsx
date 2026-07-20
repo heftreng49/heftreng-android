9932a14075ff8379a56bdbec0b2e98e5afacfb58
heftreng49 <siirgibi49@gmail.com>
2026-07-20 14:13:47 +0300

    fix: p/slug kaldirildi

--- web/src/app/p/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CmsPageClient from "./CmsPageClient"
-
-export function generateStaticParams() {
-  return []
-}
-
-export default function CmsPageRoute({ params }: { params: { slug: string } }) {
-  return <CmsPageClient slug={params.slug} />
-}
